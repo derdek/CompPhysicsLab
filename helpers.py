9bd80729a6396b22b61dc7b56f204bcdfb89f387
Denys Stashchenko <derdek02@gmail.com>
2024-10-03 22:48:41 +0300

    update

--- helpers.py
+++ helpers.py
@@ -11,21 +11,6 @@ def draw_particles(screen, particles, aspect_ration):
         )
 
 
-def snapshot(screen, x, y, N, Lx, Ly, r):
-    # Очищення екрану
-    screen.fill((0, 0, 0))
-
-    # Малювання частинок
-    for i in range(N):
-        pygame.draw.circle(screen, (0, 255, 255), (int(x[i]), int(y[i])), int(r))
-
-    # Малювання рамки
-    pygame.draw.rect(screen, (255, 0, 255), (0, 0, Lx, Ly), 1)
-
-    # Оновлення екрану
-    pygame.display.flip()
-
-
 def get_dimensions(n):
     i = int(n ** 0.5)
     while n % i != 0:
